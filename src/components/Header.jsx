571b25d5d6d032c2b0b2cc3924135ab35a512e8a
facudam <facundo89caceres@gmail.com>
2022-11-05 10:52:41 -0300

    Inicializando proyecto Typing-speed-game

---
+++ src/components/Header.jsx
@@ -0,0 +1,28 @@
+
+
+export const Header = () => {
+    return(
+        <header>
+            <div className="header__menu">
+                <span>Typing Speed Game</span>
+                <nav>
+                    <ul>
+                        <li>Play</li>
+                        <li>About</li>
+                        <li>Contact</li>
+                    </ul>
+                </nav>
+            </div>
+            <div>
+                <div>
+                    <h1>Typing Speed Game</h1>
+                    <p>¡Tipea tan rápido como puedas contra-reloj, y establece un nuevo record!</p>
+                </div>
+            </div>
+            <div>
+                <img />
+            </div>
+            
+        </header>
+    )
+}
