3d53bc48323d7c85e700050b51fe84c97ca784c8
facudam <facundo89caceres@gmail.com>
2022-11-06 20:31:00 -0300

    Estilando el  header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,21 @@
 import tipearIMG from '../images/tipearIMG.png';
 import { Nav } from './Nav';
 
+import '../styles/Header.css'
+
 export const Header = () => {
     return(
         <header>
             <Nav />
-            <div>
-                <div>
-                    <h1>Typing Speed Game</h1>
-                    <p>¡Tipea tan rápido como puedas contra-reloj, y establece un nuevo record!</p>
-                </div>
-                <div>
-                    <img src={ tipearIMG }/>
+            <div className='main-container'>
+                <div className='header__container'>
+                    <div className='header__container-title'>
+                        <h1>Typing Speed Game</h1>
+                        <p>¡Tipea tan rápido como puedas contra-reloj, y establece un nuevo record!</p>
+                    </div>
+                    <div className='header__container-img'>
+                        <img src={ tipearIMG }/>
+                    </div>
                 </div>
             </div>
             
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -2,15 +2,17 @@ import '../styles/Nav.css';
 
 export const Nav = () => {
     return(
-        <div className="header__menu">
-            <span>Typing Speed Game</span>
-            <nav>
-                <ul>
-                    <li>Play</li>
-                    <li>About</li>
-                    <li>Contact</li>
-                </ul>
-            </nav>
+        <div className='main-container-menu'>
+            <div className="header__menu">
+                <span>Typing Speed Game</span>
+                <nav>
+                    <ul>
+                        <li>Play</li>
+                        <li>About</li>
+                        <li>Contact</li>
+                    </ul>
+                </nav>
+            </div>
         </div>
     )
 }
