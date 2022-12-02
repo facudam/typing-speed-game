05b03f6b7c2264c04f9990bf12da6afe2a50233d
facudam <facundo89caceres@gmail.com>
2022-12-01 21:51:51 -0300

    Palabras de nivel 1 añadidas, función getRandomWord añadido

--- src/components/Juego.jsx
+++ src/components/Juego.jsx
@@ -3,8 +3,9 @@ import '../styles/Juego.css';
 export const Juego = () => {
     return (
         <main className="juego-container">
-            <h2>Prueba tus habilidades</h2>
-            <p>Haz click en 'comenzar' y apresurate a tipear para ganar</p>
+            <h2>Pon a prueba tus habilidades</h2>
+            <p>Haz click en 'JUGAR' y apresurate a tipear para ganar</p>
+            <button>JUGAR</button>
         </main>
     )
 }
---
+++ src/helpers/getRandomWord.js
@@ -0,0 +1,4 @@
+const getRandomWord = (palabras) => {
+    const palabraObtenida = Math.floor(Math.random() * 100) ;
+    return palabras[palabraObtenida]
+}
