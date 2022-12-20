296092ee7c5aeadca7cf85a6cbfa07b5a524d555
facudam <facundo89caceres@gmail.com>
2022-12-19 21:28:59 -0300

    Añadir inputs campos para las letras escritas por el usuario

--- src/components/PalabraEnJuego.jsx
+++ src/components/PalabraEnJuego.jsx
@@ -1,23 +1,47 @@
+import { useEffect } from "react";
 import { LetraDePalabra } from "./LetraDePalabra";
 import { palabrasDeNivelUno } from "../PALABRAS/nivel1";
 import { getRandomWord } from "../helpers/getRandomWord";
+import '../styles/PalabraEnJuego.css';
 
 export const PalabraEnJuego = () => {
 
+   useEffect(() => {
+     console.log('hola')
+   
+     
+   }, [])
+   
+   
     
+
     const letrasDePalabra = getRandomWord(palabrasDeNivelUno).toUpperCase().split('');
 
-
     return(
-        <div className="palabra-container">
-            {
-                letrasDePalabra.map(letra => (
-                    <LetraDePalabra 
-                        letra={ letra }
-                        key={ Math.random() * 1000000 }
-                    />
-                ))
-            }
+        <div>
+            <div className="palabra-container">
+                {
+                    letrasDePalabra.map(letra => (
+                        <LetraDePalabra 
+                            letra={ letra }
+                            key={ Math.random() * 1000000 }
+                        />
+                    ))
+                }
+            </div>
+            <div className="tipeo-container">
+                {
+                    letrasDePalabra.map(letra => (
+                        <input
+                            key={ Math.random() * 1000000567674 } 
+                            className="tipeo-container__letra" 
+                            type='text' 
+                            value={''}
+                        />
+                    ))
+                }
+            </div>
         </div>
+        
     )
 }
