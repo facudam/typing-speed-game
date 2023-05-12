9f93ece0f13726ca68d86bc9f7715c70bc4f5639
facudam <facundo89caceres@gmail.com>
2023-05-11 21:45:55 -0300

    Modificar fuentes de texto

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -15,7 +15,7 @@ export const Header = () => {
                     <div className='header__container-title'>
                         <h1>Typing Speed Game</h1>
                         <p>Pon a prueba tus habilidades ¡Tipea tan rápido como puedas contra-reloj, y establece un nuevo record!</p>
-                        <p>Haz click en 'Ir a juego' y ¡apresúrate a tipear para ganar!</p> 
+                        <p>Haz click en 'Ir a juego' y ¡Apresúrate a tipear para ganar!</p> 
                     </div>
                     <button
                         onClick={ () => setJuegoIniciado(true)}>
