9eb3b82fe0e7f549450ad7d18fb713dcef5037e8
facudam <facundo89caceres@gmail.com>
2023-05-13 10:50:47 -0300

    Estilos Header: titulo tipeado agregado. Boton reglas añadido

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -13,14 +13,27 @@ export const Header = () => {
             <div className='main-container'>
                 <div className='header__container'>
                     <div className='header__container-title'>
-                        <h1><span>Typing</span> <span>Speed</span> <span>Game</span></h1>
-                        <p>Pon a prueba tus habilidades ¡Tipea tan rápido como puedas contra-reloj y establece un nuevo record! Haz click en 'Ir a juego' y ¡Apresúrate a tipear para ganar!</p>
-                        <p></p> 
+                        <h1>
+                            <span>Typing</span> 
+                            <span>Speed</span> 
+                            <span>Game</span>
+                        </h1>
+                        <div>
+                            <p>Pon a prueba tus habilidades ¡Tipea tan rápido como puedas contra-reloj y establece un nuevo record! </p>
+                            <p>Haz clic en 'Ir a juego' y ¡Apresúrate a tipear para ganar!</p> 
+                        </div>
+                        
                     </div>
-                    <button
-                        onClick={ () => setJuegoIniciado(true)}>
-                        Ir a Juego
-                    </button>  
+                    <div className='btn-container'>
+                        <button>
+                            Ver Reglas
+                        </button>
+                        <button
+                            onClick={ () => setJuegoIniciado(true)}>
+                            Ir a Juego
+                        </button>
+                    </div>
+                      
                 </div>          
             </div>       
         </header>
